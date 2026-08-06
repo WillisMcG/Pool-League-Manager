365a735c1a3e718868b8f2a73bda78decdd508a1
WillisMcG <will_schulte@hotmail.com>
2026-08-05 20:37:31 -0500

    Add environment variable for Supabase key

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  },
+};
 
 export default nextConfig;
